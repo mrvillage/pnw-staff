190098c404d575bbce08296073c537f581e39c4f
Village <mr.village05@gmail.com>
2022-10-22 22:22:49 -0400

    Fix __main

--- app/routes/__main.tsx
+++ app/routes/__main.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "@remix-run/react";
+import { Background } from "~/components/Background";
 
 export default function Main() {
   return (
     <>
+      <Background />
       <Outlet />
     </>
   );
